b34c60debd5e698b5efaf9984965bd4713e94ec9
zp2946062178 <2799712178@qq.com>
2020-12-19 17:27:09 +0800

    change path of .mk

--- libpok/libc/misc/.depend.mk
+++ libpok/libc/misc/.depend.mk
@@ -1,6 +1,6 @@
 __udivdi3.o: __udivdi3.c \
- /home/tjw/rt_hw/examples/cookinggame/pr1/deployment.h \
+ /home/zp/rt_hw/examples/semaphores/pr1/deployment.h \
  ../../..//libpok/include/core/dependencies.h
 __umoddi3.o: __umoddi3.c \
- /home/tjw/rt_hw/examples/cookinggame/pr1/deployment.h \
+ /home/zp/rt_hw/examples/semaphores/pr1/deployment.h \
  ../../..//libpok/include/core/dependencies.h
